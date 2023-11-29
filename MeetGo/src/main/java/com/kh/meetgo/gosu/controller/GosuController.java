bf98179b7d1757da14f92d85d1a532ddb5da895f
cjswjddls <68465766+cjswjddls@users.noreply.github.com>
2023-11-29 18:18:56 +0900

    Update GosuController.java

--- MeetGo/src/main/java/com/kh/meetgo/gosu/controller/GosuController.java
+++ MeetGo/src/main/java/com/kh/meetgo/gosu/controller/GosuController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GosuController {
 	
+	// 고수 등록페이지로 이동
 	@RequestMapping("gosuEnrollForm.me")
 	public String gosuEnroll() {
-		
 		return "gosu/gosuEnrollForm";
 	}
 	
